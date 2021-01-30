4ca9dc5c68d3b9d6117e35c329f64a504c3e5c2c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-01-30 01:21:55 -0800

    implement route handlers from tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,52 @@
  // main.rs
-use std::io;
+#[macro_use]
+extern crate juniper;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::io;
+use std::sync::Arc;
+
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use futures::future::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+
+mod graphql_schema;
+
+use crate::graphql_schema::{create_schema, Schema};
+
+const URL = "localhost:8080";
 
 fn main() -> io::Result<()> {
-    HttpServer::new(|| {
+    let schema = std::sync::Arc::new(create_schema());
+    HttpServer::new(move || {
         App::new()
-            .route("/", web::get().to(index))
+            .data(schema.clone())
+            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
     })
-    .bind("localhost:8080")?
+    .bind(URL)?
     .run()
 }
 
-fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let res = data.execute(&st, &());
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .map_err(Error::from)
+    .and_then(|user| {
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(user))
+    })
 }
+
+fn graphiql() -> HttpResponse {
+        let html = graphiql_source(format!("http://{}/graphiql", URL));
+        HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(html)
+    }
