8234e593fe933c6e8212c73f63c54e1ec2acf6e3
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-01-30 01:16:43 -0800

    follow tutorial for setting up graphQL

---
+++ src/graphql_schema.rs
@@ -0,0 +1,57 @@
+// graphql_schema.rs
+use juniper::{EmptyMutation, RootNode};
+
+struct Jurisdiction {
+	id: i32,
+	// parent: i32,
+	name: String,
+	source: String,
+	public_link: String,
+}
+
+#[juniper::object(description = "A member of a team")]
+impl Jurisdiction {
+	pub fn id(&self) -> i32 {
+	self.id  
+	}
+
+	pub fn name(&self) -> &str {
+	self.name.as_str()
+	}
+
+	pub fn source(&self) -> &str {
+	self.source.as_str()
+	}
+
+	pub fn public_link(&self) -> &str {
+	self.public_link.as_str()
+	}
+}
+
+pub struct QueryRoot;
+
+#[juniper::object]
+impl QueryRoot {
+	fn jurisdictions() -> Vec<Jurisdiction> {
+	vec![
+		Jurisdiction {
+		id: 1,
+		name: "Test1".to_owned(),
+		source: "https://127.0.0.1/source",
+		public_link: "https://127.0.0.1/link",
+		},
+		Jurisdiction {
+		id: 2,
+		name: "Test2".to_owned(),
+		source: "https://127.0.0.1/source",
+		public_link: "https://127.0.0.1/link",
+		}
+	]
+	}
+}
+
+pub type Schema = RootNode<'static, QueryRoot, EmptyMutation<()>>;
+
+pub fn create_schema() -> Schema {
+	Schema::new(QueryRoot {}, EmptyMutation::new())
+}
