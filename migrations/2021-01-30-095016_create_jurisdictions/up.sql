16ef748d08e35a548d30ddc9cb91e3b3a16d126a
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-01-30 02:01:25 -0800

    add value to table for testing

--- migrations/2021-01-30-095016_create_jurisdictions/up.sql
+++ migrations/2021-01-30-095016_create_jurisdictions/up.sql
@@ -1,8 +1,11 @@
 -- Your SQL goes here
 
- CREATE TABLE jusrisdictions (
+CREATE TABLE jusrisdictions (
 	id INTEGER PRIMARY KEY,
 	name VARCHAR NOT NULL,
 	source VARCHAR NOT NULL,
 	public_link VARCHAR NOT NULL
 );
+
+
+INSERT INTO jusrisdictions(id, name, source, public_link) VALUES (1, 'Clackamas County', "https://www.clackamas.us/coronavirus", "https://www.clackamas.us/coronavirus" ), (2, 'Multnomah County', "multco.us/covid-vaccine", "multco.us/covid-vaccine");
